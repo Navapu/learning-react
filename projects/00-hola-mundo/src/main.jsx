cbab76ad749ae018caa446bce1a1cae424d5e89c
Alejandro Navarro <drojannp@gmail.com>
2024-12-05 11:51:45 +0100

    Creating components

--- projects/00-hola-mundo/src/main.jsx
+++ projects/00-hola-mundo/src/main.jsx
@@ -1,6 +1,16 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 
 const root = createRoot(document.getElementById('root'))
-root.render("Hola Mundo")
+const TestButton = ({text}) =>  {
+  return(
+    <button>{text}</button>
+  )
+}
+root.render(
+  <React.Fragment>
+   <TestButton text = "Boton 1"/>
+   <TestButton text = "Boton 2"/>
+  </React.Fragment>
+)
 
