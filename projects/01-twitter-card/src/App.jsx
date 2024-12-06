925b81b9a163cd3ada2489c1289b1abca3d36dae
Alejandro Navarro <drojannp@gmail.com>
2024-12-06 12:33:47 +0100

    passing functions as props

--- projects/01-twitter-card/src/App.jsx
+++ projects/01-twitter-card/src/App.jsx
@@ -1,11 +1,12 @@
 import { TwitterFollowCard } from "./TwitterFollowCard";
 
 export function App() {
+    const format = (userName) => `@${userName}`
     return (
         <div className="App">
-        <TwitterFollowCard userName="Spursito" name="Spurs" isFollowing={true} />
-        <TwitterFollowCard userName="cmariiiiiiii" name="C Marí" isFollowing={true} />
-        <TwitterFollowCard userName="c_tangana" name="Pucho" isFollowing={true} />
+        <TwitterFollowCard formatUserName = {format} userName="Spursito" name="Spurs" isFollowing={true} />
+        <TwitterFollowCard formatUserName = {format} userName="cmariiiiiiii" name="C Marí" isFollowing={true} />
+        <TwitterFollowCard formatUserName = {format} userName="c_tangana" name="Pucho" isFollowing={true} />
 
         </div>
     )
--- projects/01-twitter-card/src/TwitterFollowCard.jsx
+++ projects/01-twitter-card/src/TwitterFollowCard.jsx
@@ -1,13 +1,12 @@
-export function TwitterFollowCard({userName, name, isFollowing}) {
+export function TwitterFollowCard({formatUserName, userName, name, isFollowing}) {
     const image = `https://unavatar.io/x/${userName}`
-    console.log(isFollowing)
     return (
         <article className="main">
             <header className="tw-header">
                 <img src= {image} className="avatar"/>
                 <div className="user">
                     <strong>{name}</strong>
-                    <span className="username">{userName}</span>
+                    <span className="username">{formatUserName(userName)}</span>
                 </div>
             </header>
             <aside>
