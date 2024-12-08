f12b18d9fbe2f3e3457f21b0fd3a739152963905
Alejandro Navarro <drojannp2@gmail.com>
2024-12-08 21:18:22 +0100

    Using state to change follow button

--- projects/01-twitter-card/src/App.jsx
+++ projects/01-twitter-card/src/App.jsx
@@ -2,13 +2,13 @@ import { TwitterFollowCard } from "./TwitterFollowCard";
 
 export function App() {
     //const format = (userName) => `@${userName}`
-    const cmari = {userName:"cmariiiiiiii", name:"C Marí", isFollowing: true }
+    //const cmari = {userName:"cmariiiiiiii", name:"C Marí", isFollowing: true }
     return (
         <div className="App">
-        <TwitterFollowCard  userName="Spursito" name="Spurs" isFollowing={true} />
-        <TwitterFollowCard  {...cmari}/>
-        <TwitterFollowCard  userName="c_tangana" name="Pucho" isFollowing={true} />
-        <TwitterFollowCard  userName="Recycled_J" name="Pucho" isFollowing={true} >
+        <TwitterFollowCard  userName="Spursito" name="Spurs"/>
+        <TwitterFollowCard  userName = "cmariiiiiiii" name = "C Marí"/>
+        <TwitterFollowCard  userName="c_tangana" name="Pucho"/>
+        <TwitterFollowCard  userName="Recycled_J" name="Pucho"> 
             {/* Recycled */}
         </TwitterFollowCard>
         </div>
--- projects/01-twitter-card/src/TwitterFollowCard.jsx
+++ projects/01-twitter-card/src/TwitterFollowCard.jsx
@@ -1,5 +1,14 @@
-export function TwitterFollowCard({children, userName, name, isFollowing}) {
+import { useState } from "react"
+export function TwitterFollowCard({children, userName, name}) {
+    const [isFollowing, setIsFollowing] = useState(false)
+
     const image = `https://unavatar.io/x/${userName}`
+    const followText = isFollowing ? 'Following' : 'Follow'
+    const styleButton = isFollowing ? 'following' : 'follow'
+
+    const toggleState = () => {
+        setIsFollowing(!isFollowing)
+    }
     return (
         <article className="main">
             <header className="tw-header">
@@ -10,7 +19,7 @@ export function TwitterFollowCard({children, userName, name, isFollowing}) {
                 </div>
             </header>
             <aside>
-                <button className="follow">Seguir</button>
+                <button className={styleButton} onClick={toggleState}>{followText}</button>
             </aside>
         </article>
     )
