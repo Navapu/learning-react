99d19825682b1129490e9cb39a5b0a94b1179757
Alejandro Navarro <drojannp2@gmail.com>
2024-12-10 13:37:07 +0100

    Showing the image with the first 3 words

--- projects/02-technical-test/src/App.jsx
+++ projects/02-technical-test/src/App.jsx
@@ -1,25 +1,29 @@
 import { useEffect, useState } from "react"
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
-export function App(){
+export function App() {
     const [fact, setFact] = useState()
-    useEffect(() =>{
+    const [image, setImage] = useState()
+    useEffect(() => {
         fetch(CAT_ENDPOINT_RANDOM_FACT)
-        .then(res => res.json())
-        .then(data => {
-            const {fact} = data
-            setFact(fact)
-            
-            
-            
-            const firstsWords = fact.split(' ', 3).join(' ')
-            
-            //Another way to take the first 3 words 
-            // const firstsWords = fact.split(' ').slice(0, 3).join(' ')
+            .then(res => res.json())
+            .then(data => {
+                const { fact } = data
+                setFact(fact)
 
-            console.log(firstsWords)
-        })
+                const firstsWords = fact.split(' ', 3).join(' ')
+
+                //Another way to take the first 3 words
+                //Another way to take the first 3 words   
+                // const firstsWords = fact.split(' ').slice(0, 3).join(' ')
+
+                setImage(`https://cataas.com/cat/says/${firstsWords}?size=50&color=red`)
+                    
+            })
     }, [])
-    return(
-        <p>{fact}</p>
+    return (
+        <main>
+            <p>{fact}</p>
+            <img src={image}/>
+        </main>
     )
 }
