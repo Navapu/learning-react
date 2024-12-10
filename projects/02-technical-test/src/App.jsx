17e6e0d78ecf7eca64a5088c85fc37b19c2d18ae
Alejandro Navarro <drojannp2@gmail.com>
2024-12-10 12:46:23 +0100

    Connecting with the API

--- projects/02-technical-test/src/App.jsx
+++ projects/02-technical-test/src/App.jsx
@@ -1,5 +1,13 @@
+import { useEffect, useState } from "react"
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 export function App(){
+    const [fact, setFact] = useState()
+    useEffect(() =>{
+        fetch(CAT_ENDPOINT_RANDOM_FACT)
+        .then(res => res.json())
+        .then(data => setFact(data.fact))
+    }, [])
     return(
-        <h1>Hello world</h1>
+        <p>{fact}</p>
     )
 }
