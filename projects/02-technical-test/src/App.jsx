544b6f3553f284b4a63a5025840c142579d446e4
Alejandro Navarro <drojannp2@gmail.com>
2024-12-18 11:31:23 +0100

    Using custom hooks

--- projects/02-technical-test/src/App.jsx
+++ projects/02-technical-test/src/App.jsx
@@ -1,28 +1,38 @@
 import { useEffect, useState } from "react"
 import { getRandomFact } from "./services/fact"
 export function App() {
+
+    function useCatImage({fact}) {
+        const [image, setImage] = useState()
+
+        useEffect(() => {
+            if (fact) {
+                const firstsWords = fact.split(' ', 3).join(' ')
+                setImage(`https://cataas.com/cat/says/${firstsWords}?size=50&color=red`)
+            }
+    
+        }, [fact])
+        return image
+    }
+
     const [fact, setFact] = useState()
-    const [image, setImage] = useState()
+
     useEffect(() => {
-       getRandomFact().then(setFact)
+        getRandomFact().then(setFact)
     }, [])
 
-    useEffect(() => {
-        if (fact) {
-            const firstsWords = fact.split(' ', 3).join(' ')
-            setImage(`https://cataas.com/cat/says/${firstsWords}?size=50&color=red`)
-        }
-
-    }, [fact])
     
+
     const getFact = () => {
         getRandomFact().then(setFact)
     }
+
+    const imageUrl = useCatImage({fact})
     return (
         <main className="catContainer">
             <h1 className="catFact">{fact}</h1>
             <button onClick={getFact}>Get a new Fact</button>
-            <img src={image} className="catImage" />
+            <img src={imageUrl} className="catImage" />
         </main>
     )
 }
