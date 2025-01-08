a1307935fa8331636c5c4841160be21f9de47a4a
Alejandro Navarro <drojannp2@gmail.com>
2025-01-08 19:38:19 +0100

    Starting the movie searcher

---
+++ projects/03-movie-searcher/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="page">
+      <header>
+        <h1>Search the movie</h1>
+        <form action="">
+          <input type="text" placeholder='Fight club, Star Wars, Pulp Fiction...' />
+          <button>Search</button>
+        </form>
+      </header>
+      <main>
+
+      </main>
+    </div>
+  )
+}
+
+export default App
