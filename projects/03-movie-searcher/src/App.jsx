128f8955f64cac31418cb0458e38afdbbfba6f66
Alejandro Navarro <drojannp2@gmail.com>
2025-01-22 18:58:19 +0100

    Get the data of the form with useRef

--- projects/03-movie-searcher/src/App.jsx
+++ projects/03-movie-searcher/src/App.jsx
@@ -1,16 +1,22 @@
 import './App.css'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
-
+import { useRef } from 'react'
 
 function App() {
   const {movies: mappedMovies} = useMovies()
+  const inputRef = useRef()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(inputRef.current.value)
+  }
   return (
     <div className="page">
       <header>
         <h1>Search the movie</h1>
-        <form action="">
-          <input type="text" placeholder='Fight club, Star Wars, Pulp Fiction...' />
+        <form onSubmit={handleSubmit}>
+          <input ref={inputRef} type="text" placeholder='Fight club, Star Wars, Pulp Fiction...' />
           <button>Search</button>
         </form>
       </header>
